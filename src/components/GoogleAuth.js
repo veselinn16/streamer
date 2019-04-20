e37147b3fc5455eb1548fd93b8da5cd9492944e0
veselinn16 <veselinn16@gmail.com>
2019-04-20 20:09:08 +0300

    Initial Commit :rocket:

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class GoogleAuth extends Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client.init({
+        clientId:
+          "423369678358-6uftlo5slrg8qn69ka42mqvtqheh5va2.apps.googleusercontent.com",
+        scope: "email"
+      });
+    });
+  }
+
+  render() {
+    return <div>Google Auth</div>;
+  }
+}
+
+export default GoogleAuth;
